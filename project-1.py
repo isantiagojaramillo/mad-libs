654a5920d23b7dedfc032adecaff91adac9a7954
isantiagojaramillo <sajaramillogr@cesde.net>
2024-03-02 19:57:08 -0500

    MAD LIBS: DONE

---
+++ project-1.py
@@ -0,0 +1,8 @@
+adjetivo = input("Adjetivo: ");
+verbo1 = input("Verbo: ");
+verbo2 = input("Verbo: ");
+sustativo_plural =  input("Sustantivo (plural): ");
+
+madlib = f"¡Programar es tan {adjetivo}! Siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustativo_plural}! ";
+
+print(madlib);
